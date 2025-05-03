f0c82307cacf9fbbd0b0fc0cc418490aae0d7748
Mohd Faraz Khan <fk979765@gmail.com>
2025-05-04 02:01:48 +0530

    final working with all the funcationality of calculator is done

---
+++ README.md
@@ -0,0 +1,41 @@
+# CodeAlpha Internship Project: Calculator
+
+This is a Basic Caltulator web application developed as a part of my internship with **CodeAlpha**. 
+Built using HTML, CSS, and JavaScript with support for both mouse and keyboard inputs.
+
+## Features
+
+- Performs basic arithmetic operations: +, −, ×, ÷
+- Keyboard support for number and operator keys
+- Enter key to evaluate expressions
+- Backspace key to delete last character
+- Delete key and AC button to clear all input
+- Prevents invalid inputs like multiple consecutive operators
+- Decimal point validation
+- Responsive design
+- Dark and light mode toggle
+
+## Technologies Used
+
+- HTML5
+- CSS
+- JavaScript
+
+## How to Use
+
+- Click buttons or use keyboard to enter numbers/operators
+- Press = or Enter to calculate
+- Press DEL or Backspace to delete one character
+- Press AC or Delete key to clear all
+- Use the theme toggle to switch between dark and light mode
+
+## Project Structure
+
+- index.html – HTML structure
+- style.css – Styling and layout
+- script.js – Logic and functionality
+
+## Screenshots
+
+![Gallery Screenshot](screenshots/c1.png)
+![Lightbox screenshot](screenshots/c2.png)
--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+// Selecting the input/display area and all button elements of the calculator
 let display = document.querySelector(".display");
 let buttons= document.querySelectorAll("button");
 const operators= ["+","-","*","/"];
 let currentinput="";
-display.value="0";
+display.value="";
+// Defining keys allowed from the keyboard including digits, operators, and controls
+const allowedKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","+", "-", "*", "/", ".", "Backspace", "Delete", "Enter","ArrowLeft","ArrowRight"];
+  
+// -----------Dark Mode Section--------------
 
-document.addEventListener("keydown",(event)=>{
-});
-
-
-// dark mode
+// Selecting the theme toggle button and  the main calculator container
 let themebutton=  document.getElementById("theme");
 let calculator= document.querySelector(".calculator");
 let body= document.body;
 
+// Event listener for theme switch
 themebutton.addEventListener("click",()=>{
-
+    
+    // Toggle the 'dark' class for both calculator and body
     calculator.classList.toggle("dark");
     body.classList.toggle("dark");
+    
+    // Change button text depending on current theme
     if(body.classList.contains("dark")){
         themebutton.innerText="Light";
     }
@@ -25,70 +30,71 @@ themebutton.addEventListener("click",()=>{
     }
 });
 
+
+// Calculator Functionality, Function to Evaluate the Expression
+function caleval(){ 
+    if(currentinput===""){
+        return;
+    }
+    else{
+        try{
+            let result= eval(currentinput);
+
+            // If result is a float, limit it to 2 decimal places
+            if(result % 1 !== 0){
+                result=result.toFixed(2);
+            }
+            currentinput=result.toString();
+        }
+        catch{
+            currentinput="E";
+        }
+        display.value=currentinput;
+    }
+}
+
+//------------Button Click Logic-------------
+
+// Add click event listeners to all calculator buttons
 buttons.forEach((button) => {
     button.addEventListener("click",()=>{
-        
-        const buttontext = button.innerText;
-
-        // if(currentinput==="Error;"){
-        //     currentinput="";
-        //     display.value=currentinput;
-        // }
-
-        // if(currentinput==="0" || currentinput==="00" || currentinput==="Error;"){
-        if(currentinput==="00" || currentinput==="Error;"){
-            currentinput="0";
+        const buttontext = button.innerText;  // Capture the text inside the button clicked
+        if(currentinput==="00" || currentinput==="E"){
+            currentinput="";
             display.value=currentinput;
         }
-
-        // if(currentinput==="00"){
-        //     currentinput="0";
-        //     display.value=currentinput;
-        // }
         else if(buttontext==="Dark" || buttontext==="Light"){
             return;
         }
+
+        // AC button clears all input
         else if(buttontext==="AC"){
             currentinput="";
             display.value=currentinput;
         }
+
+        // DEL button removes last character
         else if(buttontext==="DEL"){
             currentinput=currentinput.slice(0,-1);
             display.value=currentinput;
         }
         else if(buttontext==="="){
-            if(currentinput===""){
-                return;
-            }
-            else{
-                try{
-                    let result= eval(currentinput);
-                    if(result % 1 !== 0){
-                        result=result.toFixed(2);
-                    }
-                    currentinput=result.toString();
-                    // currentinput=eval(currentinput).toString();
-                    // if(eval(currentinput) % 1 !==0){
-                    //     currentinput=eval(currentinput).toFixed(2);
-                    // }
-                }
-                catch{
-                    currentinput="Error;";
-                }
-                display.value=currentinput;
-                
-            } 
+            caleval();
         }
         else if(operators.includes(buttontext)){
-            // it allows us to input the "-" at the start of operation (a negative number)
+            // It allows us to input the "-" at the start of operation (a negative number)
             if(currentinput==="" && buttontext==="-"){
                 currentinput+=buttontext;
                 display.value=currentinput;
                 return;
             }
+
+            // Prevents operators at the beginning except minus.
             if(currentinput===""){
                 return;
             }
+
+            // Preventing double operators to be clicked at one time (like ++, **, etc.)
             const lastchar=currentinput.slice(-1);
             if(operators.includes(lastchar))
                 return;
@@ -97,25 +103,66 @@ buttons.forEach((button) => {
             display.value=currentinput;
         }          
         else{
-
+            // If dot/decimal is clicked first, prefix it with zero(0)
             if(buttontext==="." && currentinput===""){
                 currentinput="0.";
                 display.value=currentinput;
                 return;
             }
 
+            // If dot/decimal is comes after the operator then also prefix it with zero(0)
             if (buttontext === "." && operators.includes(currentinput.slice(-1))) {
                 currentinput += "0.";
                 display.value = currentinput;
                 return;
             }
-
             if (buttontext === "." && currentinput.includes(".")) {
-                return; // Prevent multiple decimal points
+                return; // Prevent multiple decimal points in the single number
             }
             currentinput+=buttontext; 
             display.value=currentinput;
         }
     });  
 });
+
+
+//-----------Keyboard Input Listener-------------
+
+document.addEventListener("keydown",(event)=>{
+    const key=event.key;
+    // If  the key is not from allowedKeys then block it and exit
+    if(!allowedKeys.includes(key)){
+        event.preventDefault();
+        return;
+    }
+
+    // If Enter is pressed then shift the focus to the equals button and Call the evaluation function and evaluate it.
+    if(key==="Enter"){
+        document.getElementById("equal").focus();
+        caleval();
+    }
+    else if(key>=0 && key<=9 || operators.includes(key)){
+        event.preventDefault();  // Prevent any unwanted default behavior of browser
+        currentinput+=key;
+        display.value=currentinput;
+    }
+
+     // If Backspace is pressed, delete last character
+    else if(key==="Backspace"){
+        event.preventDefault();// Prevent any unwanted default behavior of browser
+        currentinput=currentinput.slice(0,-1);
+        display.value=currentinput;
+    }
+
+    else if(key==="Delete"){
+        event.preventDefault();
+        currentinput="";
+        display.value=currentinput;
+    }
+});
+
+//  Cadillac CT5-V Blackwing
+
+
+
 
